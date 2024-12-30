2d30a516c64da35bfbe7cd9f4fb26b75c328f4f1
Duy Nguyen <ngdangduy2009@gmail.com>
2024-12-30 23:01:31 +1100

    Refactor Link component to use React.memo and add type annotations

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,21 +1,22 @@
 import clsx from "clsx";
 import React from "react";
 
-interface Props
-  extends React.DetailedHTMLProps<
-    React.AnchorHTMLAttributes<HTMLAnchorElement>,
-    HTMLAnchorElement
-  > {}
-
-export const Link = React.memo(({ children, ...props }: React.PropsWithChildren<Props>) => {
-  return (
-    <a
-      {...props}
-      className={clsx(props.className, "text-white hover:text-primary-500 cursor-pointer")}
-    >
-      {children}
-    </a>
-  );
-});
+export const Link = React.memo(
+  ({
+    children,
+    ...props
+  }: React.PropsWithChildren<
+    React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>
+  >) => {
+    return (
+      <a
+        {...props}
+        className={clsx(props.className, "text-white hover:text-primary-500 cursor-pointer")}
+      >
+        {children}
+      </a>
+    );
+  }
+);
 
 Link.displayName = "Link";
