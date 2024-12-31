739eadbb4d57c089d78b174aa6db0240f88c5009
Duy Nguyen <ngdangduy2009@gmail.com>
2024-12-31 16:26:17 +1100

    Update theme color in viewport metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   initialScale: 1,
   width: "device-width",
+  themeColor: '#0d0c0c'
 };
 
 export default function RootLayout({
